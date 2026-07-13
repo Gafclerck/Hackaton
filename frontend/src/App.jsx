ef2342d75db90c057198a7fd5a0fd0b452e1f099
Abdoul-Gafar AMADOU <abdoulgafaramadou938@gmail.com>
2026-07-13 09:11:09 +0000

    logique d'auth et page de login simple a modifer apres

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import Login from "./pages/Login";
 
+function Dashboard() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">Dashboard</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Ok , Welcome !
-      </h1>
-    </div>
-  )
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/dashboard" replace />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App
